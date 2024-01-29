3ea2e814044cb3b8ef0b19e9725d7a1c6546b249
Zukhriddin Jabborov <j.zuhriddin.95@gmail.com>
2024-01-29 19:31:10 +0900

    New changes added

--- KATA8/exc12.js
+++ KATA8/exc12.js
@@ -1,2 +1,8 @@
-let str = 'webbrain'
-console.log(str.repeat(10));     
+let person = {
+    name: 'Zukhriddin',
+    'full-name': 'Jabborov Zukhriddin Khoshimjon Ugli',
+    950608: 'date of birth'
+} 
+console.log(person.name);
+console.log(person['full-name']);
+console.log(person[950608]);
