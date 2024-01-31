ead1aec4d1a27c1655b8ae5b232a0b95b7456b74
Zukhriddin Jabborov <j.zuhriddin.95@gmail.com>
2024-01-31 20:08:59 +0900

    Finding the duplicates from string in array

--- KATA8/exc13.js
+++ KATA8/exc13.js
@@ -7,3 +7,21 @@ for (let i = 0; i < str.length; i++){
     }
     arr.push(str[i]);
 }
+
+
+let array = [
+    {id: 1, name: 'Zaid'},
+    {id: 2, name: 'Ali'},
+    {id: 3, name: 'Sophia'},
+    {id: 4, name: 'Muhammad'},
+]
+
+let onSearch = (input) => {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++){
+        if (array[i].name.includes(input.toUpperCase()) || (array[i].name.includes(input.toLowerCase())))
+        newArray.push(array[i])
+    }
+    console.log(newArray);
+} 
+onSearch('i')
