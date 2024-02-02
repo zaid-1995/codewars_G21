5c30c8c9a3ccb32f9cddf7d04c461027a265e941
Zukhriddin Jabborov <j.zuhriddin.95@gmail.com>
2024-02-02 23:48:16 +0900

    new updates to the functionality

--- KATA8/exc14.js
+++ KATA8/exc14.js
@@ -4,7 +4,7 @@ let b = 20;
 console.log(eval(a+b));
 console.log(`${a+b}`);
 
-// setInterval(() => {
-//     let time = new Date();
-//     console.log(`Current time: ${time.getHours} : ${time.getMinutes} : ${time.getSeconds}`); 
-// }, 1000);1
+setInterval(() => {
+    let time = new Date();
+    console.log(`Current time: ${time.getHours} : ${time.getMinutes} : ${time.getSeconds}`); 
+}, 1000);1
