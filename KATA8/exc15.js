9dca83673eaaff0f13474c2a2f56247f9398dafc
Zukhriddin Jabborov <j.zuhriddin.95@gmail.com>
2024-02-04 12:38:21 +0900

    Looping methods of array

--- KATA8/exc15.js
+++ KATA8/exc15.js
@@ -1,9 +1,32 @@
-let set = new Set([1, 2, 3, 3, 4, 5]);
+// let set = new Set([1, 2, 3, 3, 4, 5]);
 
-set.add("oops");
-set.delete(1);
-set.clear();
-let includes = set.has(3);
-let checkSize = set.size;
+// set.add("oops");
+// set.delete(1);
+// set.clear();
+// let includes = set.has(3);
+// let checkSize = set.size;
 
-console.log(set);
+// console.log(set);
+
+let students = new Set();
+
+students.add("Zaid")
+        .add("Ali")
+        .add("Muhammad");
+
+// Looping method 1
+for (let elements of students) {
+  console.log(elements);
+}
+
+// Looping method 2
+students.forEach(function (elements) {
+  console.log(elements);
+});
+
+// Looping method 3
+let studentsArray = Array.from(students);
+
+for (let i = 0; i < studentsArray.length; i++) {
+  console.log(studentsArray[i]);
+}
