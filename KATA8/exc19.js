f82b51e53b0b7c0559b1a1c1a0704ea9511b129d
Zukhriddin Jabborov <j.zuhriddin.95@gmail.com>
2024-02-08 01:01:56 +0900

    Getting the age of object by using Recursive function

--- KATA8/exc19.js
+++ KATA8/exc19.js
@@ -22,5 +22,22 @@ let getAge = (obj) => {
   }
 };
 
-getAge(user);
-console.log(sum);
+// getAge(user);
+// console.log(sum);
+
+let person = {
+  firstName: "Zaid",
+  lastName: "Jabborov",
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+
+  set fullName(value) {
+    let values = value.split(" ");
+    this.firstName = values[0];
+    this.lastName = values[1];
+  },
+};
+person.fullName = "Muhammad Ali";
+console.log(person.fullName);
