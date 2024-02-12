3ca30a4ab629c95f9d63af34856211c1def9e8e9
Zukhriddin Jabborov <j.zuhriddin.95@gmail.com>
2024-02-12 19:41:15 +0900

    Destructure in Nested Object

---
+++ KATA8/exc20.js
@@ -0,0 +1,22 @@
+// Destucture in Nested Object
+
+let obj = {
+  name: "Zaid",
+  occupation: "Developer",
+  age: 28,
+
+  friend: {
+    ismi: "Murod",
+    kasbi: "Student",
+    yoshi: 27,
+  },
+};
+
+let {name, occupation, age,  friend: { ismi, kasbi, yoshi} } = obj;
+
+console.log(name);
+console.log(occupation);
+console.log(age);
+console.log(ismi);
+console.log(kasbi);
+console.log(yoshi);
