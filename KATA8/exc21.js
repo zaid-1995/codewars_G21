0222345967824214a92ea732f10ebb6786c48c8f
Zukhriddin Jabborov <j.zuhriddin.95@gmail.com>
2024-02-12 20:10:11 +0900

    Array destructure 100% project based example

--- KATA8/exc21.js
+++ KATA8/exc21.js
@@ -1,9 +1,24 @@
 // Destructure in Array
 
-let arr = ['Zaid', 'Murod', 'Ali'];
+let arr = ["Zaid", "Murod", "Ali"];
 
-let[ name1, name2, name3 ] = arr;
+let [name1, name2, name3] = arr;
 
 console.log(name1);
 console.log(name2);
 console.log(name3);
+
+// Array destructure 100% project based example
+
+let users = [
+  { id: 1, name: "Zaid", occupation: "Developer" },
+  { id: 2, name: "Ali", occupation: "Student" },
+  { id: 3, name: "Murod", occupation: "Teacher" },
+  { id: 4, name: "Muhammad", occupation: "CEO" },
+];
+
+users.map(({id, name, occupation}) => {
+    console.log(id);
+    console.log(name);
+    console.log(occupation);
+});
