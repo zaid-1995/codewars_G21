762579d23c85771972ec6803afd8879711a46879
Zukhriddin Jabborov <j.zuhriddin.95@gmail.com>
2024-02-13 23:16:06 +0900

    new updated

--- KATA8/exc22.js
+++ KATA8/exc22.js
@@ -1,1 +1,16 @@
-examplegit 
+let person = {
+    firstName: "Zaid",
+    lastName: "Jabborov",
+  
+    get fullName() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+  
+    set fullName(value) {
+      let values = value.split(" ");
+      this.firstName = values[0];
+      this.lastName = values[1];
+    },
+  };
+  person.fullName = "Muhammad Ali";
+  console.log(person.fullName);
