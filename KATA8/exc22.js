ed26c756b5735d3235b21699541c7226be0cf840
Zukhriddin Jabborov <j.zuhriddin.95@gmail.com>
2024-02-14 21:14:31 +0900

    Asynchrome in setTimeout function

--- KATA8/exc22.js
+++ KATA8/exc22.js
@@ -1,16 +1,7 @@
-let person = {
-    firstName: "Zaid",
-    lastName: "Jabborov",
-  
-    get fullName() {
-      return `${this.firstName} ${this.lastName}`;
-    },
-  
-    set fullName(value) {
-      let values = value.split(" ");
-      this.firstName = values[0];
-      this.lastName = values[1];
-    },
-  };
-  person.fullName = "Muhammad Ali";
-  console.log(person.fullName);
+console.log(1, "st iteration");
+
+setTimeout(() => {
+  console.log(2, 'nd iteration');
+}, 3000);
+
+console.log(3, 'rd iteration');
