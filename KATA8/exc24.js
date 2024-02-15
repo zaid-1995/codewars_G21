2db9a1ac96093f7f88633bd699a8d6816a7ce919
Zukhriddin Jabborov <j.zuhriddin.95@gmail.com>
2024-02-15 19:55:43 +0900

    Facebook login page

--- KATA8/exc24.js
+++ KATA8/exc24.js
@@ -1,8 +1,27 @@
 try {
-    let test = add (10, 11)
-} catch (error){
-    console.log(error)
-    console.log(error.stack)
-    console.log(error.name)
-    console.log(error.message)
+  let test = add(10, 11);
+} catch (error) {
+  // console.log(error)
+  // console.log(error.stack)
+  console.log(error.name);
+  // console.log(error.message)
 }
+
+let users = {
+  username: "zaid95",
+  password: "19950608",
+};
+
+let facebook = (pw, usn, callback) => {
+  setTimeout(() => {
+    if (users.password === pw && users.username === usn) {
+      callback("Welcome to your Facebook page.");
+    } else {
+      callback("username or password is wrong.");
+    }
+  }, 2000);
+};
+
+let user = facebook("19950608", "zaid95", (data) => {
+  console.log(data);
+});
