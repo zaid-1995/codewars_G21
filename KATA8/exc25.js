efabadc39a20929342a304c854ba77b71ca1679c
Zukhriddin Jabborov <j.zuhriddin.95@gmail.com>
2024-03-05 01:25:37 +0900

    getting an array of numbers, return the sum of all of the positives ones

--- KATA8/exc25.js
+++ KATA8/exc25.js
@@ -11,3 +11,10 @@ function positiveSum(arr) {
 let arr = [1, -4, 7, 12];
 let result = positiveSum(arr);
 console.log(result);
+
+function boolToWord( bool ){
+    return bool ? "Yes" : "No";
+}
+
+console.log(boolToWord(true));
+console.log(boolToWord(false));
