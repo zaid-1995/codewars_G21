173c5fb844f12a92e79e5f18b6eb629fc31dafff
Zukhriddin Jabborov <j.zuhriddin.95@gmail.com>
2024-03-07 23:56:57 +0900

    updated

--- KATA7/task2.js
+++ KATA7/task2.js
@@ -31,13 +31,3 @@ var number = function(arr) {
       }
       return newArr;
     }
-
-    // final method
-    var number=function(array){
-        if (array.length == 0) return [];
-        
-        for (let i=0; i<array.length; ++i)
-          array[i] = (i+1) + ': ' + array[i];
-          
-        return array;
-      }
